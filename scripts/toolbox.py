0d5c71b3e443bed60b1761e4a77444d2c7a9d45f
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2017-12-31 14:13:20 -0200

    Creating of of the function DNF
    The function shorten now returns a sorted list. This list also has the original sentence in it.

--- scripts/toolbox.py
+++ scripts/toolbox.py
@@ -6,15 +6,16 @@ bl = boolean.BooleanAlgebra()
 
 def shorten(sentence):
     k = set()
+    k.add(sentence)
     for i in sentence.args:
         k.add(i)
         if i.iscanonical:
             continue
         else:    
             k.update(shorten(i))
-    return k
+    return sorted(k, key=lambda x: len(x.__str__()))
 
-# Assumes sentence will be the set returned by shorten
+# Assumes sentence will be the list returned by shorten
 def truthtable(sentence):
     x, y = [], []
     for i in sentence:
@@ -24,4 +25,26 @@ def truthtable(sentence):
             y.append(i.__str__())
     
     return Truths(x, y)
-        
+
+# Receives the table returned by truthtable
+def DNF(table):
+    tablelist = table.table()
+    parts = []
+    
+    for row in tablelist[1:]:
+        if row[-1] == 1:
+            temp = []
+
+            for i in range(len(table.base)):
+                if row[i] == 0:
+                    temp.append('~'+tablelist[0][i])
+                else:
+                    temp.append(tablelist[0][i])
+            
+            part = '&'.join(temp)
+            parts.append('(' + part + ')')
+    
+    dnf = '|'.join(parts)
+    
+    return dnf
+            
