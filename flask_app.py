c08f3c263a562ef83ef84ce1431fd76bcd039b4e
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2018-06-04 16:25:08 -0300

    Added a separate page for the results. Now the button that is pressed is not passed as a parameter anymore.

--- flask_app.py
+++ flask_app.py
@@ -12,53 +12,55 @@ hdr = []
 
 @app.route('/', methods=["GET"])
 def index():
-    try:
-        if request.args["button"] == "Resolver":
-            stc = request.args["entrada"]
-            table = toolbox.truthtable(stc).table()
+    print("-- Homepage --")
+    return render_template("main_page.html")
 
-            d = toolbox.DNF(stc)
-            c = toolbox.CNF(stc)
-
-            hd = table[0] + ['POS', 'SOP']
-
-            for i in range(len(table[1:])):
-                empty = 1
-                for j in c:
-                    if j[0] == i:
-                        table[i+1].append(j[1])
-                        empty = 0
-
-                if empty:
-                    table[i+1].append("")
-                
-                empty = 1
-                for j in d:
-                    if j[0] == i:
-                        table[i+1].append(j[1])
-                        empty = 0                       
-
-                if empty:
-                    table[i+1].append("")
-
-            
-
-            bd = table[1:]
-            km = toolbox.kmap(stc)
-            kgrp = [sorted([(j[0]+1, j[1]+1) for j in i]) for i in km.groups]
-            print(kgrp)
+@app.route('/random', methods=["GET"])
+def aleatorio():
+    hdr = toolbox.generate(4)
+    print("-- Aleatorio --\n{}".format(hdr))
+    return render_template("main_page.html", holder=hdr)
 
 
-            return render_template("main_page.html", sentence=stc, head=hd, body=bd, dnf=d, cnf=c, km=km.map, kg=kgrp)
+@app.route('/results', methods=["GET"])
+def resultado():
+    print("-- Resultado --\n")
+    # stc = request.form.get("entrada")
+    stc = request.args["entrada"]
+    table = toolbox.truthtable(stc).table()
 
-        elif request.args["button"] == "Aleatorio":
-            hdr = toolbox.generate(4)
-            return render_template("main_page.html", holder=hdr)
+    d = toolbox.DNF(stc)
+    c = toolbox.CNF(stc)
 
-    except KeyError as e:
-        return render_template("main_page.html")
+    hd = table[0] + ['POS', 'SOP']
+
+    for i in range(len(table[1:])):
+        empty = 1
+        for j in c:
+            if j[0] == i:
+                table[i+1].append(j[1])
+                empty = 0
+
+        if empty:
+            table[i+1].append("")
+        
+        empty = 1
+        for j in d:
+            if j[0] == i:
+                table[i+1].append(j[1])
+                empty = 0                       
+
+        if empty:
+            table[i+1].append("")
+
+    
+
+    bd = table[1:]
+    km = toolbox.kmap(stc)
+    kgrp = [sorted([(j[0]+1, j[1]+1) for j in i]) for i in km.groups]
 
 
+    return render_template("results.html", sentence=stc, head=hd, body=bd, dnf=d, cnf=c, km=km.map, kg=kgrp)
 
 if __name__ == '__main__':
     app.run()
